8f3c8e9dcbfcece13b2f146f88d2b6add8002097
Istvan Acs <istvan.acs89@gmail.com>
2018-08-12 15:42:32 +0100

    Adding new item to the budget controller

--- src/_asset/js/app.js
+++ src/_asset/js/app.js
@@ -1,27 +1,58 @@
 // budget controller
 let budgetController = ( () => {
-   let Expense = (id, description, value) => {
-       this.id = id;
-       this.description = description;
-       this.value = value;
-   };
-
-   let Income = (id, description, value) => {
-       this.id = id;
-       this.description = description;
-       this.value = value;
+    let Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
     };
 
-   let data = {
-       allitems: {
-           exp: [],
-           inc: []
-       },
-       totals: {
-           exp: 0,
-           inc: 0
-       }
-   };
+    let Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    let data = {
+        allitems: {
+            exp: [],
+            inc: []
+        },
+         totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+
+    return {
+        addItem: (type, des, val) => {
+            let newItem, ID;
+
+            //create new ID
+            if(data.allitems[type].length > 0){
+                ID = data.allitems[type][data.allitems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            //create new item based 'inc' or 'exp' type
+            if (type === 'exp'){
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            // push it into our data structure
+            data.allitems[type].push(newItem);
+
+            // return the new element
+            return newItem;
+
+        },
+
+        testing: () => {
+            console.log(data);
+        }
+    }
 
 })();
 
@@ -66,9 +97,12 @@ let controller = ( (budgetCtrl, UICtrl) => {
     };
 
     let ctrlAddItem = () => {
+        let input, newItem;
         // 1. Get the field input data
-        let input = UICtrl.getinput();
+        input = UICtrl.getinput();
+
         // 2. Add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         // 3. Add the item to the UI
         // 4. Calculate the budget
         // 5. Display the budget on the UI
