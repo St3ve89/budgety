37b3bf523c722c51188e6a95aa9dac4161503872
Istvan Acs <istvan.acs89@gmail.com>
2018-08-12 12:36:46 +0100

    Create methods and make them work together

--- src/_asset/js/app.js
+++ src/_asset/js/app.js
@@ -1,6 +1,29 @@
-let budgetController = (()=>{
+let budgetController = (() => {
     let x = 23;
     let add = a => {
         return x + a;
     }
+    return {
+        publicTest: b => {
+            return add(b);
+        }
+    }
 })();
+
+let UIController = (() => {
+
+    // some code
+
+})();
+
+let controller = ((budgetCtrl, UICtrl) => {
+
+    let z = budgetCtrl.publicTest(5);
+
+    return {
+        anotherPublic: () => {
+            console.log(z);
+        }
+    }
+
+})(budgetController, UIController);
